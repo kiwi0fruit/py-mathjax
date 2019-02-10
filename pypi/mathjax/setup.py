a25d5c7f1293fbefc49a61ec196d733af279aabd
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-02-10 11:46:02 +0700

    commit

--- pypi/mathjax/setup.py
+++ pypi/mathjax/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mathjax',
-    version='0.1.1',
+    version='0.1.2',
 
     description='Stub that prevents name clash with https://github.com/conda-forge/mathjax-feedstock For MathJax see https://github.com/kiwi0fruit/py-mathjax',
     url='https://github.com/kiwi0fruit/py-mathjax',
