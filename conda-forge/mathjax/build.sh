6599098c22564109d89cc90526fc2bb4cb28d53b
kiwi0fruit <peter.zagubisalo@gmail.com>
2019-12-10 18:15:10 +0700

    3.0.0

--- conda-forge/mathjax/build.sh
+++ conda-forge/mathjax/build.sh
@@ -2,25 +2,32 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-mathjax="$PREFIX/lib/mathjax"
+if [[ "$OSTYPE" == "msys" ]]; then
+    mathjax="$(cygpath "${LIBRARY_LIB}")/mathjax"
+    bin_dir="$(cygpath "$SCRIPTS")"
+else
+    mathjax="$PREFIX/lib/mathjax"
+    bin_dir="$PREFIX/bin"
+fi
+
+
+for di in ./*
+do
+    if [[ -d "$di" && ! -d ./mathjax ]]; then
+        mv "$di" ./mathjax
+    fi
+done
+
+
 mkdir -p "$mathjax"
+mv ./mathjax/es5 "$mathjax/"
+mv ./mathjax/LICENSE ./
 
-mv config "$mathjax/"
-mv docs "$mathjax/"
-mv extensions "$mathjax/"
-mv fonts "$mathjax/"
-mv jax "$mathjax/"
-mv localization "$mathjax/"
-mv test "$mathjax/"
-mv unpacked "$mathjax/"
 
-rm *.md ".gitignore" ".travis.yml" "bower.json" "composer.json" "latest.js" "package.json" || exit 1
-cwd="$(pwd)"
-cp -r "$cwd/." "$mathjax/"
-cd "$mathjax"
-rm *.sh LICENSE
-cd "$cwd"
-
-mkdir -p "$PREFIX/bin"
-cp "${RECIPE_DIR}/mathjax-path" "$PREFIX/bin/"
-chmod +x "$PREFIX/bin/mathjax-path"
+mkdir -p "${bin_dir}"
+cp "${RECIPE_DIR}/mathjax-path" "${bin_dir}/"
+if [[ "$OSTYPE" == "msys" ]]; then
+    cp "${RECIPE_DIR}/mathjax-path.bat" "${bin_dir}/"
+else
+    chmod +x "${bin_dir}/mathjax-path"
+fi
--- conda-forge/mathjax/mathjax-path.bat
+++ conda-forge/mathjax/mathjax-path.bat
@@ -1,9 +1,9 @@
 @echo off
 REM script_dir == [env_root]\Scripts\
 set "script_dir=%~dp0"
-set "mathjax=%script_dir:~0,-9%\Library\lib\mathjax\MathJax.js"
+set "mathjax=%script_dir:~0,-9%\Library\lib\mathjax\es5\"
 if exist "%mathjax%" (
     echo %mathjax%
 ) else (
-    echo Error: "%mathjax%" was not found. 1>&2
+    echo Error: "%mathjax%" dir was not found. 1>&2
 )
